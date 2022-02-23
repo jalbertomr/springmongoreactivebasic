427d87b33a4ea961c76a0805d906a6d634126339
jalbertomr <jalbertomr@yahoo.com>
2022-02-23 13:46:27 -0600

    Or Ambiguity solved with @Qualifier("OrangeJuice")

--- src/main/java/com/bext/spring/depInj/impl/OrangeJuice.java
+++ src/main/java/com/bext/spring/depInj/impl/OrangeJuice.java
@@ -1,12 +1,12 @@
 package com.bext.spring.depInj.impl;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.bext.spring.depInj.Drink;
 
 @Component
-@Primary
+@Qualifier("OrangeJuice")
 public class OrangeJuice implements Drink {
 
 	@Override
--- src/main/java/com/bext/spring/service/EatService.java
+++ src/main/java/com/bext/spring/service/EatService.java
@@ -2,6 +2,7 @@ package com.bext.spring.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.bext.spring.depInj.Drink;
@@ -12,7 +13,7 @@ public class EatService {
     
     private Drink drink;
 
-	public EatService(Drink drink) {
+	public EatService(@Qualifier("OrangeJuice") Drink drink) {
 		this.drink = drink;
 		LOG.info("Constructor EatService({})", drink.getName());
 	}
